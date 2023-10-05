18d0e02168e9f0ca95816db86094a55fef415b77
Renato Marques <renatodelimamarques@gmail.com>
2023-10-05 08:30:39 -0300

    💄 Woo product loop

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Loop Add to Cart
  *
@@ -15,22 +16,24 @@
  * @version     3.3.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH'))
+{
+   exit;
 }
+return;
 
 global $product;
 
 echo apply_filters(
-	'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
-	sprintf(
-		'<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
-		esc_url( $product->add_to_cart_url() ),
-		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
-		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
-		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
-		esc_html( $product->add_to_cart_text() )
-	),
-	$product,
-	$args
+   'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+   sprintf(
+      '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+      esc_url($product->add_to_cart_url()),
+      esc_attr(isset($args['quantity']) ? $args['quantity'] : 1),
+      esc_attr(isset($args['class']) ? $args['class'] : 'button'),
+      isset($args['attributes']) ? wc_implode_html_attributes($args['attributes']) : '',
+      esc_html($product->add_to_cart_text())
+   ),
+   $product,
+   $args
 );
