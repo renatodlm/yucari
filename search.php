96d53ab054bfe31201f91a130989fcea75306bde
Renato Marques <renato@buildbox.com.br>
2023-12-06 09:45:51 -0300

    🔨 search and style

--- search.php
+++ search.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying search results pages
  *
@@ -10,44 +11,64 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		<?php if ( have_posts() ) : ?>
+   <?php if (have_posts()) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'yucari' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
+      <div class="container woocommerce">
+         <header class="page-header mt-10">
+            <div>
+               <h1 class="entry-title">
+                  <?php
+                  /* translators: %s: search query. */
+                  printf(esc_html__('Search Results for: %s', 'yucari'), '<span class="normal-case">' . get_search_query() . '</span>');
+                  ?>
+               </h1>
+               <div>
+                  <?php get_search_form() ?>
+               </div>
+            </div>
+         </header><!-- .page-header -->
+         <ul class="products columns-3">
+         <?php
+         /* Start the Loop */
+         while (have_posts()) :
+            the_post();
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+            /**
+             * Run the loop for the search to output the results.
+             * If you want to overload this in a child theme then include a file
+             * called content-search.php and that will be used instead.
+             */
+            wc_get_template_part('content', 'product');
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
+         endwhile;
 
-			endwhile;
 
-			the_posts_navigation();
 
-		else :
+      else :
 
-			get_template_part( 'template-parts/content', 'none' );
+         get_template_part('template-parts/content', 'none');
 
-		endif;
-		?>
+      endif;
+         ?>
+         </ul>
+         <?php
 
-	</main><!-- #main -->
+         global $wp_query;
+
+         $big = 999999999;
+
+         echo paginate_links(array(
+            'base'    => str_replace($big, '%#%', esc_url(get_pagenum_link($big))),
+            'format'  => '?paged=%#%',
+            'current' => max(1, get_query_var('paged')),
+            'total'   => $wp_query->max_num_pages,
+         )); ?>
+      </div>
+
+</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
